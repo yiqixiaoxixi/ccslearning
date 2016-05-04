9e86a83724daab97c4f1b3c9ad237d2925b89ee5
shekchiang <sheckchiang@gmail.com>
2016-05-04 14:13:09 +0800

    delete C6747_SDRAM

--- C6747_SDRAM/Debug/all/C6747.pp
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-all/C6747.obj: ../all/C6747.c
-all/C6747.obj: ../all/C6747.h
-
-../all/C6747.c: 
-../all/C6747.h: 
--- C6747_SDRAM/Debug/all/init.pp
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-all/init.obj: ../all/init.c
-all/init.obj: ../all/C6747.h
-
-../all/init.c: 
-../all/C6747.h: 
